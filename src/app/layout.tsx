269c717459abef96e3b37749a93b04fd972d69af
Manvendra <manvendra.kumar7@gmail.com>
2024-10-15 18:25:23 +0530

    adding navigation menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import './globals.css';
+import { DollarSignIcon } from 'lucide-react';
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu';
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -19,8 +28,42 @@ export default function RootLayout({
 					'min-h-screen, bg-background font-sans antialiased',
 					inter.variable
 				)}>
-				{children}
+				<header className='flex py-2 border-b bg-card gap-2'>
+					<div className='flex items-center font-semibold px-8'>
+						<DollarSignIcon className='size-6' />
+						<span>Personal Finance Tools</span>
+					</div>
+					<NavigationMenu>
+						<NavigationMenuList>
+							<NavigationMenuItem>
+								<NavigationMenuTrigger>Calculator</NavigationMenuTrigger>
+								<NavigationMenuContent>
+									<ul className='gap-2'>
+										<ListItem title='SIP Calculator' />
+										<ListItem title='SWP Calculator' />
+									</ul>
+								</NavigationMenuContent>
+							</NavigationMenuItem>
+						</NavigationMenuList>
+					</NavigationMenu>
+				</header>
+				<main className='container my-6 mx-auto'>{children}</main>
 			</body>
 		</html>
+	);
+}
+
+function ListItem({ title }: { title: string }) {
+	return (
+		<li>
+			<NavigationMenuLink href='/calculator/compound-interest'>
+				<a
+					className={
+						'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground'
+					}>
+					<div className='font-medium whitespace-nowrap'>{title}</div>
+				</a>
+			</NavigationMenuLink>
+		</li>
 	);
 }
