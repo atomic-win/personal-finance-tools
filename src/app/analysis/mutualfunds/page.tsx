21d933a3ad4695d866f985019df12ff0028ab8bd
Manvendra <manvendra.kumar7@gmail.com>
2024-10-17 19:28:31 +0530

    add SIP input card to mutualfunds analysis page

--- src/app/analysis/mutualfunds/page.tsx
+++ src/app/analysis/mutualfunds/page.tsx
@@ -1,7 +1,13 @@
-export default function MutualFundsPage() {
+'use client';
+import SIPInputCard from './sip-card';
+
+export default function SIPPage() {
 	return (
 		<div className='container mx-auto p-2'>
-			<h1 className='text-2xl font-bold mb-4'>Mutual Funds</h1>
+			<h1 className='text-2xl font-bold mb-4'>Mutual Funds Analysis</h1>
+			<div className='grid grid-cols-3 gap-4'>
+				<SIPInputCard />
+			</div>
 		</div>
 	);
 }
---
+++ src/app/analysis/mutualfunds/sip-card.tsx
@@ -0,0 +1,125 @@
+'use client';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const SIPCalculatorSchema = z.object({
+	lumpsumAmount: z.coerce.number().min(0, {
+		message: 'Lumpsum Amount cannot be less than 0',
+	}),
+	monthlyInvestment: z.coerce
+		.number()
+		.min(1, { message: 'Monthly Investment cannot be less than 1' }),
+	annualStepUpPercent: z.coerce.number().min(-99, {
+		message: 'Annual Step-Up Percent cannot be less than or equal to -100%',
+	}),
+	annualInterestPercent: z.coerce.number().min(-99, {
+		message: 'Annual Interest Percent cannot be less than or equal to -100%',
+	}),
+	numberOfYears: z.coerce
+		.number()
+		.min(1, { message: 'Investment Duration cannot be less than 1 year' }),
+});
+
+const formFields = [
+	{
+		name: 'lumpsumAmount',
+		label: 'Lumpsum Amount',
+		description: 'Enter the lumpsum amount',
+	},
+	{
+		name: 'monthlyInvestment',
+		label: 'Monthly Installment',
+		description: 'Enter the monthly installment',
+	},
+	{
+		name: 'annualInterestPercent',
+		label: 'Annual Interest Rate (%)',
+		description: 'Enter the annual interest rate',
+	},
+	{
+		name: 'annualStepUpPercent',
+		label: 'Annual Step-Up (%)',
+		description: 'Enter the annual step-up percentage',
+	},
+	{
+		name: 'numberOfYears',
+		label: 'Investment Duration (Years)',
+		description: 'Enter the investment duration in years',
+	},
+];
+
+export default function SIPInputCard() {
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const { replace } = useRouter();
+
+	const form = useForm<z.infer<typeof SIPCalculatorSchema>>({
+		resolver: zodResolver(SIPCalculatorSchema),
+		defaultValues: {
+			lumpsumAmount: Number(searchParams.get('lumpsumAmount') || 0),
+			monthlyInvestment: Number(searchParams.get('monthlyInvestment') || 500),
+			annualStepUpPercent: Number(
+				searchParams.get('annualStepUpPercent') || 10
+			),
+			annualInterestPercent: Number(
+				searchParams.get('annualInterestPercent') || 10
+			),
+			numberOfYears: Number(searchParams.get('numberOfYears') || 10),
+		},
+	});
+
+	function onChange(values: z.infer<typeof SIPCalculatorSchema>) {
+		const params = new URLSearchParams(searchParams);
+		for (const key in values) {
+			params.set(key, values[key as keyof typeof values].toString());
+		}
+		replace(`${pathname}?${params.toString()}`);
+	}
+
+	return (
+		<Card className='mx-auto my-2 p-2 rounded-lg shadow-md w-full col-start-3'>
+			<CardHeader>
+				<CardTitle>SIP Parameters</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<Form {...form}>
+					<form onChange={form.handleSubmit(onChange)} className='space-y-4'>
+						{formFields.map((formField) => (
+							<FormField
+								key={formField.name}
+								control={form.control}
+								name={
+									formField.name as keyof z.infer<typeof SIPCalculatorSchema>
+								}
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>{formField.label}</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormDescription>{formField.description}</FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						))}
+					</form>
+				</Form>
+			</CardContent>
+		</Card>
+	);
+}
