96db268e0e9be0eb6fa5e879dd888790f2fb0ebc
Manvendra <manvendra.kumar7@gmail.com>
2024-10-19 03:59:53 +0530

    Refactor useReturnQueries and useRollingReturnsQuery in mutualfunds.ts

--- src/components/hooks/mutualfunds.ts
+++ src/components/hooks/mutualfunds.ts
@@ -108,6 +108,10 @@ export function useReturnQueries(
 	investmentDuration: PresetTimeDurations,
 	lookbackDuration: PresetTimeDurations
 ) {
+	const earliestDate = DateTime.max(
+		...mutualfunds.map((mf) => DateTime.fromISO(mf.earliestDate))
+	);
+
 	return useQueries({
 		queries: mutualfunds.map((mutualfund) => ({
 			queryKey: [
@@ -127,7 +131,7 @@ export function useReturnQueries(
 
 				const returns: MutualFundReturn[] = [];
 				for (
-					let date = startDate;
+					let date = DateTime.max(startDate, earliestDate);
 					date <= endDate;
 					date = date.plus({ days: 1 })
 				) {
@@ -178,13 +182,14 @@ export function useRollingReturnsQuery(
 				.minus(getLuxonDuration(rollingWindow))
 				.plus({ days: 1 });
 
-			const earliestDate = DateTime.fromISO(mutualfund.earliestDate);
-
 			const returns = [];
 			for (
-				let date = endDate;
-				date >= startDate && date >= earliestDate;
-				date = date.minus({ days: 1 })
+				let date = DateTime.max(
+					startDate,
+					DateTime.fromISO(mutualfund.earliestDate)
+				);
+				date <= endDate;
+				date = date.plus({ days: 1 })
 			) {
 				returns.push(evaluateMutualFund(mutualfund.navs, rollingWindow, date));
 			}
