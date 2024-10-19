06847860346d28c86a11f7e743bdf45ccfa0aada
Manvendra <manvendra.kumar7@gmail.com>
2024-10-19 19:37:09 +0530

    using sidebar instead of navigation menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,14 @@
 import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import './globals.css';
-import { DollarSignIcon } from 'lucide-react';
-import {
-	NavigationMenu,
-	NavigationMenuContent,
-	NavigationMenuItem,
-	NavigationMenuList,
-	NavigationMenuTrigger,
-} from '@/components/ui/navigation-menu';
-import Link from 'next/link';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -40,65 +37,19 @@ export default function RootLayout({
 					'min-h-screen, bg-background font-sans antialiased',
 					inter.variable
 				)}>
-				<header className='flex py-2 border-b bg-card gap-2'>
-					<Link href='/' className='flex items-center font-semibold px-8'>
-						<DollarSignIcon className='size-6' />
-						<span>Personal Finance Tools</span>
-					</Link>
-					<NavigationMenu>
-						<NavigationMenuList>
-							<NavigationMenuItem>
-								<NavigationMenuTrigger>Calculator</NavigationMenuTrigger>
-								<NavigationMenuContent>
-									<ul className='gap-2'>
-										<ListItem title='SIP Calculator' href='/calculator/sip' />
-										<ListItem title='SWP Calculator' href='/calculator/swp' />
-										<ListItem
-											title='SIP-SWP Calculator'
-											href='/calculator/sip-swp'
-										/>
-									</ul>
-								</NavigationMenuContent>
-							</NavigationMenuItem>
-						</NavigationMenuList>
-					</NavigationMenu>
-					<NavigationMenu>
-						<NavigationMenuList>
-							<NavigationMenuItem>
-								<NavigationMenuTrigger>Analysis</NavigationMenuTrigger>
-								<NavigationMenuContent>
-									<ul className='gap-2'>
-										<ListItem
-											title='Mutual Funds'
-											href='/analysis/mutualfunds'
-										/>
-									</ul>
-								</NavigationMenuContent>
-							</NavigationMenuItem>
-						</NavigationMenuList>
-					</NavigationMenu>
-				</header>
-				<main className='container my-6 mx-auto'>
-					<QueryClientProvider client={queryClient}>
-						{children}
-						<ReactQueryDevtools />
-					</QueryClientProvider>
-				</main>
+				<SidebarProvider>
+					<AppSidebar />
+					<SidebarInset>
+						<main className='container my-6 mx-auto'>
+							<QueryClientProvider client={queryClient}>
+								<SidebarTrigger />
+								{children}
+								<ReactQueryDevtools />
+							</QueryClientProvider>
+						</main>
+					</SidebarInset>
+				</SidebarProvider>
 			</body>
 		</html>
-	);
-}
-
-function ListItem({ title, href }: { title: string; href: string }) {
-	return (
-		<li>
-			<Link
-				href={href}
-				className={
-					'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground'
-				}>
-				<div className='font-medium whitespace-nowrap'>{title}</div>
-			</Link>
-		</li>
 	);
 }
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,92 @@
+import { DollarSignIcon } from 'lucide-react';
+
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarMenuSub,
+	SidebarMenuSubButton,
+	SidebarMenuSubItem,
+	SidebarRail,
+} from '@/components/ui/sidebar';
+
+const data = [
+	{
+		title: 'Calculators',
+		url: '#',
+		items: [
+			{
+				title: 'SIP Calculator',
+				url: '/calculator/sip',
+			},
+			{
+				title: 'SWP Calculator',
+				url: '/calculator/swp',
+			},
+			{
+				title: 'SIP-SWP Calculator',
+				url: '/calculator/sip-swp',
+			},
+		],
+	},
+	{
+		title: 'Analysis',
+		url: '#',
+		items: [
+			{
+				title: 'Mutual Funds',
+				url: '/analysis/mutualfunds',
+			},
+		],
+	},
+];
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+	return (
+		<Sidebar {...props}>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton size='lg' asChild>
+							<a href='#'>
+								<div className='flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground'>
+									<DollarSignIcon className='size-4' />
+								</div>
+								<div className='flex flex-col gap-0.5 leading-none'>
+									<span className='font-semibold'>Personal Finance Tools</span>
+								</div>
+							</a>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarMenu>
+						{data.map((item) => (
+							<SidebarMenuItem key={item.title}>
+								<SidebarMenuButton className='font-medium'>
+									{item.title}
+								</SidebarMenuButton>
+								<SidebarMenuSub>
+									{item.items.map((item) => (
+										<SidebarMenuSubItem key={item.title}>
+											<SidebarMenuSubButton asChild>
+												<a href={item.url}>{item.title}</a>
+											</SidebarMenuSubButton>
+										</SidebarMenuSubItem>
+									))}
+								</SidebarMenuSub>
+							</SidebarMenuItem>
+						))}
+					</SidebarMenu>
+				</SidebarGroup>
+			</SidebarContent>
+			<SidebarRail />
+		</Sidebar>
+	);
+}
