ecd1344c02ee6aa0af4691d75057cfd68032b771
Manvendra <manvendra.kumar7@gmail.com>
2024-10-21 01:09:15 +0530

    add sorting to PortfolioPerInstrumentTypeSection

--- src/features/investments/components/PortfolioPerInstrumentTypeSection.tsx
+++ src/features/investments/components/PortfolioPerInstrumentTypeSection.tsx
@@ -9,33 +9,172 @@ import {
 	displayPercentage,
 } from '@/features/investments/lib/utils';
 import { DataTable } from '@/components/ui/data-table';
+import { Button } from '@/components/ui/button';
+import { ArrowDown, ArrowUp, ArrowUpDown } from 'lucide-react';
 
 const columns: ColumnDef<InstrumentTypePortfolio>[] = [
 	{
-		header: 'Instrument Type',
-		accessorFn: (data) => displayInstrumentType(data.id as InstrumentType),
+		accessorKey: 'instrumentType',
+		header: () => {
+			return (
+				<Button variant='ghost' className='p-0'>
+					Instrument Type
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			return (
+				<div className='text-left font-medium'>
+					{displayInstrumentType(row.original.id as InstrumentType)}
+				</div>
+			);
+		},
 	},
 	{
-		header: 'Invested Value',
-		accessorFn: (data) =>
-			displayCurrencyAmount(data.currency, data.initialAmount),
+		accessorKey: 'initialAmount',
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+					className='p-0'>
+					Invested Value
+					{column.getIsSorted() === 'asc' && <ArrowDown className='h-4 w-4' />}
+					{column.getIsSorted() === 'desc' && <ArrowUp className='h-4 w-4' />}
+					{column.getIsSorted() === false && (
+						<ArrowUpDown className='h-4 w-4' />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			return (
+				<div className='text-right font-medium'>
+					{displayCurrencyAmount(
+						row.original.currency,
+						row.original.initialAmount
+					)}
+				</div>
+			);
+		},
+		sortingFn: (a, b) => {
+			return a.original.initialAmount - b.original.initialAmount;
+		},
 	},
 	{
-		header: 'Invested Value (%)',
-		accessorFn: (data) => displayPercentage(data.initialAmountPercent),
+		accessorKey: 'initialAmountPercent',
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+					className='p-0'>
+					Invested Value (%)
+					{column.getIsSorted() === 'asc' && <ArrowDown className='h-4 w-4' />}
+					{column.getIsSorted() === 'desc' && <ArrowUp className='h-4 w-4' />}
+					{column.getIsSorted() === false && (
+						<ArrowUpDown className='h-4 w-4' />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			return (
+				<div className='text-right font-medium'>
+					{displayPercentage(row.original.initialAmountPercent)}
+				</div>
+			);
+		},
+		sortingFn: (a, b) => {
+			return a.original.initialAmountPercent - b.original.initialAmountPercent;
+		},
 	},
 	{
-		header: 'Current Value',
-		accessorFn: (data) =>
-			displayCurrencyAmount(data.currency, data.currentAmount),
+		accessorKey: 'currentAmount',
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+					className='p-0'>
+					Current Value
+					{column.getIsSorted() === 'asc' && <ArrowDown className='h-4 w-4' />}
+					{column.getIsSorted() === 'desc' && <ArrowUp className='h-4 w-4' />}
+					{column.getIsSorted() === false && (
+						<ArrowUpDown className='h-4 w-4' />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			return (
+				<div className='text-right font-medium'>
+					{displayCurrencyAmount(
+						row.original.currency,
+						row.original.currentAmount
+					)}
+				</div>
+			);
+		},
+		sortingFn: (a, b) => {
+			return a.original.currentAmount - b.original.currentAmount;
+		},
 	},
 	{
-		header: 'Current Value (%)',
-		accessorFn: (data) => displayPercentage(data.currentAmountPercent),
+		accessorKey: 'currentAmountPercent',
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+					className='p-0'>
+					Current Value (%)
+					{column.getIsSorted() === 'asc' && <ArrowDown className='h-4 w-4' />}
+					{column.getIsSorted() === 'desc' && <ArrowUp className='h-4 w-4' />}
+					{column.getIsSorted() === false && (
+						<ArrowUpDown className='h-4 w-4' />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			return (
+				<div className='text-right font-medium'>
+					{displayPercentage(row.original.currentAmountPercent)}
+				</div>
+			);
+		},
+		sortingFn: (a, b) => {
+			return a.original.currentAmountPercent - b.original.currentAmountPercent;
+		},
 	},
 	{
-		header: 'XIRR (%)',
-		accessorFn: (data) => displayPercentage(data.xirrPercent),
+		accessorKey: 'xirrPercent',
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+					className='p-0'>
+					XIRR (%)
+					{column.getIsSorted() === 'asc' && <ArrowDown className='h-4 w-4' />}
+					{column.getIsSorted() === 'desc' && <ArrowUp className='h-4 w-4' />}
+					{column.getIsSorted() === false && (
+						<ArrowUpDown className='h-4 w-4' />
+					)}
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			return (
+				<div className='text-right font-medium'>
+					{displayPercentage(row.original.xirrPercent)}
+				</div>
+			);
+		},
+		sortingFn: (a, b) => {
+			return a.original.xirrPercent - b.original.xirrPercent;
+		},
 	},
 ];
 
