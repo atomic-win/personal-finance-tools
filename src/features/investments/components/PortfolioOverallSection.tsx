e7666ee69404e51a646c7ab0dff50a26e084fc69
Manvendra <manvendra.kumar7@gmail.com>
2024-10-21 02:14:55 +0530

    Refactor PortfolioOverallSection to use createNormalColumnDef

--- src/features/investments/components/PortfolioOverallSection.tsx
+++ src/features/investments/components/PortfolioOverallSection.tsx
@@ -1,26 +1,29 @@
 import { ColumnDef } from '@tanstack/react-table';
 import { OverallPortfolio } from '@/features/investments/lib/types';
-import { DataTable } from '@/components/ui/data-table';
+import { createNormalColumnDef, DataTable } from '@/components/ui/data-table';
 import {
 	displayCurrencyAmount,
 	displayPercentage,
 } from '@/features/investments/lib/utils';
 
 const columns: ColumnDef<OverallPortfolio>[] = [
-	{
-		header: 'Invested Value',
-		accessorFn: (data) =>
+	createNormalColumnDef({
+		accessorKey: 'initialAmount',
+		headerText: 'Initial Amount',
+		cellTextFn: (data) =>
 			displayCurrencyAmount(data.currency, data.initialAmount),
-	},
-	{
-		header: 'Current Value',
-		accessorFn: (data) =>
+	}),
+	createNormalColumnDef({
+		accessorKey: 'currentAmount',
+		headerText: 'Current Amount',
+		cellTextFn: (data) =>
 			displayCurrencyAmount(data.currency, data.currentAmount),
-	},
-	{
-		header: 'XIRR (%)',
-		accessorFn: (data) => displayPercentage(data.xirrPercent),
-	},
+	}),
+	createNormalColumnDef({
+		accessorKey: 'xirrPercent',
+		headerText: 'XIRR (%)',
+		cellTextFn: (data) => displayPercentage(data.xirrPercent),
+	}),
 ];
 
 export default function PortfolioOverallSection({
