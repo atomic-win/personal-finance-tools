203a93aa5244a8a1607134f15c80f9198242cee0
Manvendra <manvendra.kumar7@gmail.com>
2024-12-17 01:10:21 +0530

    fixind prod build errors

---
+++ src/features/analyzers/components/MutualFunds.tsx
@@ -0,0 +1,42 @@
+'use client';
+import {
+	useMutualFundListQuery,
+	useMutualFundQueries,
+} from '@/features/analyzers/hooks/mutualfunds';
+import MutualFundsReturnsChart from '@/features/analyzers/components/MutualFundsReturnsChart';
+import MutualFundsRollingReturnsTable from '@/features/analyzers/components/MutualFundsRollingReturnsTable';
+import MutualFundsForm from '@/features/analyzers/components/MutualFundsForm';
+import { useSearchParams } from 'next/navigation';
+
+export default function MutualFunds() {
+	const searchParams = useSearchParams();
+	const { data: mutualFundList } = useMutualFundListQuery();
+
+	const addedMutualFundResults = useMutualFundQueries(
+		searchParams.getAll('mfSchemeCode').map(Number)
+	);
+
+	if (!mutualFundList || !mutualFundList.length) {
+		return null;
+	}
+
+	const addedMutualfunds = (addedMutualFundResults || [])
+		.filter((r) => r.isSuccess)
+		.map((r) => r.data!)
+		.filter((mf) => mf !== null && !!mf.schemeName);
+
+	return (
+		<div className='grid grid-cols-3 gap-4'>
+			<div className='col-span-2'>
+				<MutualFundsReturnsChart mutualfunds={addedMutualfunds} />
+				<MutualFundsRollingReturnsTable mutualfunds={addedMutualfunds} />
+			</div>
+			<div>
+				<MutualFundsForm
+					mutualFundList={mutualFundList}
+					addedMutualFunds={addedMutualfunds}
+				/>
+			</div>
+		</div>
+	);
+}
