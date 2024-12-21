d7b6bc0f5b712444850cd9996ec26b60257fc209
Manvendra <manvendra.kumar7@gmail.com>
2024-12-21 20:16:19 +0530

    home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@/components/ui/button';
-
 export default function HomePage() {
 	return (
 		<div className='text-center container my-4 mx-auto'>
-			<h1 className='text-3xl mb-4'>Fancy Home Page</h1>
-			<div className='flex gap-2 justify-center'>
-				<Button>Click me</Button>
-			</div>
+			<h1 className='text-3xl'>Personal Finance Tools</h1>
+			<p className='mt-4'>
+				Welcome to the personal finance tools app. This app contains a set of
+				calculators and analysis tools to help you assess returns on
+				investments.
+			</p>
 		</div>
 	);
 }
