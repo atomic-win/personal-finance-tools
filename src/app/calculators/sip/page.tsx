c9b89c0df3383045f06a8596f79f795aee37fe3d
Manvendra <manvendra.kumar7@gmail.com>
2024-12-21 20:27:45 +0530

    SIP calculator page done

--- src/app/calculators/sip/page.tsx
+++ src/app/calculators/sip/page.tsx
@@ -18,7 +18,17 @@ export default function SIPPage() {
 	return (
 		<div className='container mx-auto p-2'>
 			<h1 className='text-2xl font-bold mb-4'>SIP Calculator</h1>
-			<Button onClick={addCalculator}>Add Calculator</Button>
+			<p>
+				A Systematic Investment Plan (SIP) is a disciplined and convenient way
+				to invest in mutual funds. With SIP, you can invest a fixed amount at
+				regular intervals, allowing you to benefit from rupee cost averaging and
+				the power of compounding. Use our SIP calculator to estimate your future
+				wealth based on your investment amount, tenure, and expected returns,
+				and plan your financial goals effectively.
+			</p>
+			<div className='flex justify-end mb-4'>
+				<Button onClick={addCalculator}>Add Calculator</Button>
+			</div>
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
 				{calculators.map((id, index) => (
 					<SIPCalculatorCard
