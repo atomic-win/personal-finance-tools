dca9b3244ce074fb07e0444f8780cc611b911d50
Manvendra <manvendra.kumar7@gmail.com>
2024-12-22 19:48:14 +0530

    SIP+SWP Page done

--- src/app/calculators/sip-swp/page.tsx
+++ src/app/calculators/sip-swp/page.tsx
@@ -1,7 +1,48 @@
 'use client';
-import SipSwpCalculatorCard from '@/features/calculators/components/SipSwpCalculatorCard';
-import { SipSwpCalculator } from '@/features/calculators/lib/types';
+import {
+	SipSwpCalculator,
+	sipSwpCalculatorSchema,
+} from '@/features/calculators/lib/types';
 import CalculatorPage from '@/features/calculators/components/CalculatorPage';
+import SipSwpCalculatorResult from '@/features/calculators/components/SipSwpCalculatorResult';
+
+const formFields = [
+	{
+		name: 'lumpsumAmount',
+		label: 'Lumpsum Amount',
+		description: 'Enter the lumpsum amount',
+	},
+	{
+		name: 'monthlySipInvestmentAmount',
+		label: 'Monthly SIP Investment',
+		description: 'Enter the monthly SIP investment amount',
+	},
+	{
+		name: 'annualSipStepUpPercent',
+		label: 'Annual SIP Step-Up (%)',
+		description: 'Enter the annual SIP step-up percentage',
+	},
+	{
+		name: 'annualInterestPercent',
+		label: 'Annual Interest Rate (%)',
+		description: 'Enter the annual interest rate',
+	},
+	{
+		name: 'numberOfSipYears',
+		label: 'SIP Investment Duration (Years)',
+		description: 'Enter the SIP investment duration in years',
+	},
+	{
+		name: 'monthlySwpWithdrawalAmount',
+		label: 'Monthly SWP Withdrawal',
+		description: 'Enter the monthly SWP withdrawal amount',
+	},
+	{
+		name: 'annualInflationPercent',
+		label: 'Annual Inflation Rate (%)',
+		description: 'Enter the annual inflation rate',
+	},
+];
 
 export default function Page() {
 	const pageDescription = `The SIP + SWP Calculator helps you plan your investments and withdrawals. Systematic Investment Plan (SIP) allows you to invest a fixed amount regularly, while Systematic Withdrawal Plan (SWP) enables you to withdraw a fixed amount periodically. Use this tool to simulate and manage your financial goals effectively.`;
@@ -11,7 +52,9 @@ export default function Page() {
 			calculatorName='SIP+SWP'
 			pageDescription={pageDescription}
 			type='sip-swp'
-			CalculatorCard={SipSwpCalculatorCard}
+			calculatorSchema={sipSwpCalculatorSchema}
+			formFields={formFields}
+			CalculatorResult={SipSwpCalculatorResult}
 		/>
 	);
 }
