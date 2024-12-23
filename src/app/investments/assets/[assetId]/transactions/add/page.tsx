6905233e7c0802a7a03784527fa29f5e418da0e2
Manvendra <manvendra.kumar7@gmail.com>
2024-12-23 14:31:58 +0530

    AddTransaction page breadcrumb

--- src/app/investments/assets/[assetId]/transactions/add/page.tsx
+++ src/app/investments/assets/[assetId]/transactions/add/page.tsx
@@ -1,16 +1,53 @@
 'use client';
+import SidebarTriggerWithBreadcrumb from '@/components/SidebarTriggerWithBreadcrumb';
 import { Card } from '@/components/ui/card';
 import AddTransactionForm from '@/features/investments/components/forms/AddTransactionForm';
+import { withAssetPortfolios } from '@/features/investments/components/hoc/withAssetPortfolios';
+import withAssets from '@/features/investments/components/hoc/withAssets';
+import withCurrency from '@/features/investments/components/hoc/withCurrency';
+import withInstruments from '@/features/investments/components/hoc/withInstruments';
+import withTransactions from '@/features/investments/components/hoc/withTransactions';
+import { AssetPortfolio } from '@/features/investments/lib/types';
 
 export default function Page({ params }: { params: { assetId: string } }) {
+	const assetId = params.assetId;
+
+	const WithAddTransactionFormWrapper = withAssets(
+		withInstruments(
+			withCurrency(
+				withTransactions(withAssetPortfolios(AddTransactionFormWrapper))
+			)
+		)
+	);
+
+	return <WithAddTransactionFormWrapper assetIds={[assetId]} latest={true} />;
+}
+
+function AddTransactionFormWrapper({
+	portfolios,
+}: {
+	portfolios: AssetPortfolio[];
+}) {
+	const asset = portfolios[0];
+
 	return (
-		<div className='container mx-auto p-2'>
-			<h1 className='text-2xl font-bold mb-4'>Add Transaction</h1>
-			<div className='w-1/3 mx-auto'>
-				<Card className='p-4 pb-0'>
-					<AddTransactionForm assetId={params.assetId} />
+		<>
+			<SidebarTriggerWithBreadcrumb
+				breadcrumbs={[
+					{ title: 'Investments', href: '#' },
+					{ title: 'Assets', href: '/investments/assets' },
+					{ title: asset.assetName, href: `/investments/assets/${asset.id}` },
+					{
+						title: 'Add Transaction',
+						href: `/investments/assets/${asset.id}/transactions/add`,
+					},
+				]}
+			/>
+			<div className='container mx-auto p-2'>
+				<Card className='p-4 pb-0 w-1/3 mx-auto'>
+					<AddTransactionForm asset={asset} />
 				</Card>
 			</div>
-		</div>
+		</>
 	);
 }
