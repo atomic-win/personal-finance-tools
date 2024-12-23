6419588f92145a7f216d8ee4a31bfc594bf3814d
Manvendra <manvendra.kumar7@gmail.com>
2024-12-23 12:30:22 +0530

    Add lodash for data manipulation and update mutual fund query to use uniqBy

--- src/features/analyzers/hooks/mutualfunds.ts
+++ src/features/analyzers/hooks/mutualfunds.ts
@@ -8,6 +8,7 @@ import { getLuxonDuration } from '@/features/analyzers/lib/utils';
 import { useQueries, useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { DateTime } from 'luxon';
+import _ from 'lodash';
 
 const mfApiClient = axios.create({
 	baseURL: 'https://api.mfapi.in',
@@ -18,9 +19,9 @@ export function useMutualFundListQuery() {
 	return useQuery({
 		queryKey: ['mutualfunds', 'list'],
 		queryFn: async () => {
-			return (await mfApiClient.get('mf')).data;
+			return (await mfApiClient.get('mf')).data as MutualFund[];
 		},
-		select: (data) => data as MutualFund[],
+		select: (data) => _.uniqBy(data, (mf) => mf.schemeCode),
 		staleTime: 1000 * 60 * 60 * 24, // 24 hours
 		refetchInterval: 1000 * 60 * 60 * 24, // 24 hours
 		refetchIntervalInBackground: true,
