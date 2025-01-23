49ffb77a681a1bf7fe2a70d26d35619f247d54af
Manvendra <manvendra.kumar7@gmail.com>
2025-01-23 23:24:04 +0530

    Google Site Verification (#46)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<head>
+				<meta
+					name='google-site-verification'
+					content='ZQlI6Wndfdb4ajaLwhp7Z2rC2KdpKeoJuKYG9WWvI-Q'
+				/>
 				<script
 					async
 					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7570289096550601'
