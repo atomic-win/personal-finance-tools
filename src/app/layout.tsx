88c295d762b852c1f3d5852325457f1f5142c461
Manvendra <manvendra.kumar7@gmail.com>
2025-01-23 22:47:13 +0530

    Adding Google Ads (#45)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<head>
+				<script
+					async
+					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7570289096550601'
+					crossOrigin='anonymous'></script>
+			</head>
 			<body
 				className={cn(
 					'min-h-screen, bg-background font-sans antialiased',
