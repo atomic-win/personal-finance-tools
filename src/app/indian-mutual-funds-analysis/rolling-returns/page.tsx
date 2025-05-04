1bb4421b4b7ebae9111271bf91eb0f5960a1c01a
Manvendra <manvendra.kumar7@gmail.com>
2025-05-04 16:46:41 +0530

    Refactor rolling returns pages to use ReturnsPage component for improved structure and maintainability

--- src/app/indian-mutual-funds-analysis/rolling-returns/page.tsx
+++ src/app/indian-mutual-funds-analysis/rolling-returns/page.tsx
@@ -1,39 +1,6 @@
 'use client';
-import SidebarTriggerWithBreadcrumb from '@/components/SidebarTriggerWithBreadcrumb';
-import ReturnsPageContainer from '@/features/indian-mutual-funds-analysis/components/ReturnsPageContainer';
-import { Suspense } from 'react';
+import ReturnsPage from '@/features/indian-mutual-funds-analysis/components/ReturnsPage';
 
 export default function Page() {
-	return (
-		<>
-			<title>Mutual Funds Rolling Returns</title>
-			<meta
-				name='keywords'
-				content='Mutual Funds, Rolling Returns, CAGR, Investment Analysis, Financial Planning'
-			/>
-			<SidebarTriggerWithBreadcrumb
-				breadcrumbs={[
-					{ title: 'Indian Mutual Funds Analysis', href: '', disabled: true },
-					{
-						title: 'Rolling Returns',
-						href: '/indian-mutual-funds-analysis/rolling-returns',
-					},
-				]}
-			/>
-			<div className='px-4 space-y-2'>
-				<h1 className='text-2xl font-bold'>Indian Mutual Funds Analysis</h1>
-				<h2 className='text-lg font-semibold'>Rolling Returns</h2>
-				<p>
-					Analyze CAGR (Compound Annual Growth Rate) rolling returns of Indian
-					Mutual Funds. Understand long-term fund performance across different
-					time frames to make informed investment decisions. Discover trends,
-					evaluate consistency, and compare funds to identify those that align
-					with your financial goals.
-				</p>
-				<Suspense>
-					<ReturnsPageContainer returnType='simple' />
-				</Suspense>
-			</div>
-		</>
-	);
+	return <ReturnsPage returnType='simple' />;
 }
--- src/features/indian-mutual-funds-analysis/components/ReturnsPage.tsx
+++ src/features/indian-mutual-funds-analysis/components/ReturnsPage.tsx
@@ -8,12 +8,49 @@ import RollingReturnsTableCard from '@/features/indian-mutual-funds-analysis/com
 import SelectMutualFundsCard from '@/features/indian-mutual-funds-analysis/components/SelectMutualFundsCard';
 import { useSearchParams } from 'next/navigation';
 import { ReturnType } from '@/features/indian-mutual-funds-analysis/lib/types';
+import SidebarTriggerWithBreadcrumb from '@/components/SidebarTriggerWithBreadcrumb';
+import { Suspense } from 'react';
 
-export default function ReturnsPageContainer({
+export default function ReturnsPage({
 	returnType,
 }: {
 	returnType: ReturnType;
 }) {
+	return (
+		<>
+			<title>Mutual Funds Rolling Returns</title>
+			<meta
+				name='keywords'
+				content='Mutual Funds, Rolling Returns, CAGR, Investment Analysis, Financial Planning'
+			/>
+			<SidebarTriggerWithBreadcrumb
+				breadcrumbs={[
+					{ title: 'Indian Mutual Funds Analysis', href: '', disabled: true },
+					{
+						title: 'Rolling Returns',
+						href: '/indian-mutual-funds-analysis/rolling-returns',
+					},
+				]}
+			/>
+			<div className='px-4 space-y-2'>
+				<h1 className='text-2xl font-bold'>Indian Mutual Funds Analysis</h1>
+				<h2 className='text-lg font-semibold'>Rolling Returns</h2>
+				<p>
+					Analyze CAGR (Compound Annual Growth Rate) rolling returns of Indian
+					Mutual Funds. Understand long-term fund performance across different
+					time frames to make informed investment decisions. Discover trends,
+					evaluate consistency, and compare funds to identify those that align
+					with your financial goals.
+				</p>
+				<Suspense>
+					<ReturnsPageContainer returnType={returnType} />
+				</Suspense>
+			</div>
+		</>
+	);
+}
+
+function ReturnsPageContainer({ returnType }: { returnType: ReturnType }) {
 	const searchParams = useSearchParams();
 	const { data: mutualFundList } = useMutualFundListQuery();
 
