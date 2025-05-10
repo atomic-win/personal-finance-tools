d28a79b4c210661bf3711272b1c7931c8c49669a
Manvendra <manvendra.kumar7@gmail.com>
2025-05-10 14:20:37 +0530

    Fixing build error (#85)

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,6 @@ const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	distDir: 'build',
 	output: 'standalone',
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.worker\.ts$/,
-			use: { loader: 'worker-loader', options: { esModule: true } },
-		});
-		return config;
-	},
 };
 
 export default nextConfig;
