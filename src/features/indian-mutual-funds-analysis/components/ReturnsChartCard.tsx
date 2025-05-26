5f239528442e3f55168fae4d7296856825f7504f
Manvendra <manvendra.kumar7@gmail.com>
2025-05-26 21:53:10 +0530

    Add unit prop to ChartTooltipContent in ReturnsChart for percentage display

--- src/features/indian-mutual-funds-analysis/components/ReturnsChartCard.tsx
+++ src/features/indian-mutual-funds-analysis/components/ReturnsChartCard.tsx
@@ -198,7 +198,10 @@ function ReturnsChart(
 						style: { textAnchor: 'middle' },
 					}}
 				/>
-				<ChartTooltip cursor={true} content={<ChartTooltipContent />} />
+				<ChartTooltip
+					cursor={true}
+					content={<ChartTooltipContent unit='%' />}
+				/>
 				{mutualfunds.map((mutualfund) => (
 					<Line
 						key={mutualfund.schemeCode}
