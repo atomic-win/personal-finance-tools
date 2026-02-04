f1176e396e274e5a5d1a2be5301d16397a99e72c
Manvendra <manvendra.kumar7@gmail.com>
2026-02-04 19:35:22 +0530

    Remove unused Google Analytics integration and related dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import '@/app/globals.css';
 import Providers from '@/components/Providers';
-import { GoogleAnalytics } from '@next/third-parties/google';
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -16,16 +15,6 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<head>
-				<meta
-					name='google-site-verification'
-					content='ZQlI6Wndfdb4ajaLwhp7Z2rC2KdpKeoJuKYG9WWvI-Q'
-				/>
-				<script
-					async
-					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7570289096550601'
-					crossOrigin='anonymous'></script>
-			</head>
 			<body
 				className={cn(
 					'min-h-screen, bg-background font-sans antialiased',
@@ -33,7 +22,6 @@ export default function RootLayout({
 				)}>
 				<Providers>{children}</Providers>
 			</body>
-			<GoogleAnalytics gaId='G-NL8V8WZQN1' />
 		</html>
 	);
 }
