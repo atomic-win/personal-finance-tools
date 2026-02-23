23c8d753334c27e9c86cf71c371b2d28ffefc0b6
Manvendra <manvendra.kumar7@gmail.com>
2026-02-23 19:59:53 +0530

    Reorder components in ReturnsPageContainer for improved layout and user experience

--- src/features/indian-mutual-funds-analysis/components/ReturnsPage.tsx
+++ src/features/indian-mutual-funds-analysis/components/ReturnsPage.tsx
@@ -99,8 +99,8 @@ function ReturnsPageContainer({ returnType }: { returnType: ReturnType }) {
 		<div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
 			<div className='order-2 md:order-1 md:col-span-2 space-y-4'>
 				<ReturnsForm returnRequest={returnsRequest} />
-				<ReturnsChartCard returnRequest={returnsRequest} />
 				<RollingReturnsTableCard returnRequest={returnsRequest} />
+				<ReturnsChartCard returnRequest={returnsRequest} />
 			</div>
 			<div className='order-1 md:order-2'>
 				<SelectMutualFundsCard />
