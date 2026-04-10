a993bacb0ee32a14d98ace997b2f7a694073b7b6
Manvendra <manvendra.kumar7@gmail.com>
2026-04-11 03:30:47 +0530

    Refactor NavChartCard rendering for improved readability

--- src/features/indian-mutual-funds-analysis/components/NavPage.tsx
+++ src/features/indian-mutual-funds-analysis/components/NavPage.tsx
@@ -49,10 +49,7 @@ function NavChartPageContainer() {
 	return (
 		<div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
 			<div className='order-2 md:order-1 md:col-span-2 space-y-4'>
-				<NavChartCard
-					timeRange={timeRange}
-					onTimeRangeChange={setTimeRange}
-				/>
+				<NavChartCard timeRange={timeRange} onTimeRangeChange={setTimeRange} />
 			</div>
 			<div className='order-1 md:order-2'>
 				<SelectMutualFundsCard />
