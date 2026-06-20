90c4d14aa1b2470b093b28b097ed482b705892dd
Manvendra <manvendra.kumar7@gmail.com>
2026-06-20 16:51:02 +0530

    chore: add nitro and nitropack dependencies; update Vite config to in… (#130)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
 	resolve: {
 		tsconfigPaths: true,
 	},
-	plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+	plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
 });
